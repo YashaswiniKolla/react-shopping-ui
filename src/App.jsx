5c98460047159a124ec20a4008e68fb38d77dc1e
Yash <yashaswinikurubaa@gmail.com>
2024-05-13 17:28:34 +0530

    shopping react ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css'
+import Mainpage from "./shoppingfolder/pages/Mainpage";
+
+const App = () => {
+  return(
+    <Mainpage />
+  )
+}
+
+export default App;
